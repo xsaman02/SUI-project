69790b7f7f6f9524d5074302d76ace8652e99695
Honza <xsaman02@vutbr.cz>
2020-10-12 11:00:52 +0200

    first (dump) ai.

--- dicewars-master/dicewars/ai/template.py
+++ dicewars-master/dicewars/ai/template.py
@@ -1,6 +1,8 @@
 import logging
 
 from dicewars.client.ai_driver import BattleCommand, EndTurnCommand
+from dicewars.ai.utils import possible_attacks, probability_of_successful_attack
+
 
 
 class AI:
@@ -8,5 +10,13 @@ class AI:
         self.player_name = player_name
         self.logger = logging.getLogger('AI')
 
+
     def ai_turn(self, board, nb_moves_this_turn, nb_turns_this_game, time_left):
+        attacks = list(possible_attacks(board, self.player_name))
+
+        for source, target in attacks:
+            prob = probability_of_successful_attack(board, source.get_name(), target.get_name())
+            if prob > 0.6:
+                return BattleCommand(source.get_name(), target.get_name())
+
         return EndTurnCommand()
--- dicewars-master/scripts/utils.py
+++ dicewars-master/scripts/utils.py
@@ -5,8 +5,6 @@ import tempfile
 import numpy as np
 import random
 
-sys.path.insert(0, "./")
-sys.path.insert(0, "./dicewars")
 from dicewars.server.summary import GameSummary
 
 
